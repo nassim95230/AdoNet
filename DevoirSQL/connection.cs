6c5398da387ad781c8bab3b31c88c3dc91f4f2a8
Nass <nasfahdine@outlook.com>
2018-10-19 11:09:22 +0200

    initial commit

---
+++ DevoirSQL/connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Ado
+{
+    static class connection
+    {
+        static private MySqlConnection cnx;
+
+        public static MySqlConnection Cnx
+        {
+            get 
+            { 
+                return connection.cnx; 
+            }
+        }
+        static private string sConnection;
+
+        static connection()
+        {
+            sConnection = "host=localhost; database=tennis; user=root; password=siojjr";
+            cnx = new MySqlConnection(sConnection);
+        }
+    }
+}
